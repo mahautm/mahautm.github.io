e7f2e132e5942eecd1854a64fc8f32b0c0911270
mahautm <mateo.mahaut@upf.edu>
2023-05-23 11:04:21 +0200

    cssstuff

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Add an event listener to each tab link
+const tabLinks = document.querySelectorAll('.tabs a');
+tabLinks.forEach(tabLink => {
+    tabLink.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // Remove the 'active' class from all tab links
+        tabLinks.forEach(link => link.classList.remove('active'));
+
+        // Add the 'active' class to the clicked tab link
+        this.classList.add('active');
+
+        // Get the target tab content ID from the href attribute
+        const targetId = this.getAttribute('href').substring(1);
+
+        // Hide all tab contents
+        const tabContents = document.querySelectorAll('.tab-content');
+        tabContents.forEach(content => {
+            content.style.display = 'none';
+        });
+
+        // Show the target tab content
+        document.getElementById(targetId).style.display = 'block';
+    });
+});
